caf86033dafd9052f49bd670af6247d00f716f72
Mitchell Elliott <mitchellelliott@mac.com>
2021-04-12 12:59:16 -0500

    Addition of ADSR labels

--- SupersawDesignerPrototype/Source/UI/adsrComponent.cpp
+++ SupersawDesignerPrototype/Source/UI/adsrComponent.cpp
@@ -13,17 +13,11 @@
 
 AdsrComponent::AdsrComponent(juce::AudioProcessorValueTreeState& apvts)
 {
-    using SliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
     
-    attackAttachment = std::make_unique<SliderAttachment>(apvts, "ATTACK" , attackSlider);
-    decayAttachment = std::make_unique<SliderAttachment>(apvts, "DECAY" , decaySlider);
-    sustainAttachment = std::make_unique<SliderAttachment>(apvts, "SUSTAIN" , sustainSlider);
-    releaseAttachment = std::make_unique<SliderAttachment>(apvts, "RELEASE" , releaseSlider);
-    
-    setSliderParams(attackSlider);
-    setSliderParams(decaySlider);
-    setSliderParams(sustainSlider);
-    setSliderParams(releaseSlider);
+    setSliderWithLabel(attackSlider, attackLabel, apvts, "ATTACK", attackAttachment);
+    setSliderWithLabel(decaySlider, decayLabel, apvts, "DECAY", decayAttachment);
+    setSliderWithLabel(sustainSlider, sustainLabel, apvts, "SUSTAIN", sustainAttachment);
+    setSliderWithLabel(releaseSlider, releaseLabel, apvts, "RELEASE", releaseAttachment);
 
 }
 
@@ -49,20 +43,42 @@ void AdsrComponent::resized()
     const auto bounds = getLocalBounds().reduced(10);
     const auto padding = 10;
     const auto sliderWidth = bounds.getWidth() / 4 - padding;
-    const auto sliderHeight = bounds.getHeight()- (padding);
+    const auto sliderHeight = bounds.getHeight()- (45);
     const auto sliderStartX = padding;
-    const auto sliderStartY = padding*2;
+    const auto sliderStartY = 50;
+    const auto labelYOffset = 20;
+    const auto labelHeight = 20;
+    const auto labelStart = sliderStartY - labelYOffset;
+    
     
     attackSlider.setBounds(sliderStartX, sliderStartY, sliderWidth, sliderHeight);
+    attackLabel.setBounds (attackSlider.getX(), labelStart, sliderWidth, labelHeight);
+    
     decaySlider.setBounds(attackSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
+    decayLabel.setBounds(decaySlider.getX(), labelStart, sliderWidth, labelHeight);
+    
     sustainSlider.setBounds(decaySlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
+    sustainLabel.setBounds(sustainSlider.getX(), labelStart, sliderWidth, labelHeight);
+    
     releaseSlider.setBounds(sustainSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
-
+    releaseLabel.setBounds(releaseSlider.getX(), labelStart, sliderWidth, labelHeight);
+    
 }
 
-void AdsrComponent::setSliderParams (juce::Slider& slider)
+using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+
+void AdsrComponent::setSliderWithLabel (juce::Slider& slider, juce::Label& label, juce::AudioProcessorValueTreeState& apvts, juce::String paramId, std::unique_ptr<Attachment>& attachment)
 {
+    
     slider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
-    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 60, 25);
+    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 50, 25);
     addAndMakeVisible(slider);
+    
+    attachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(apvts, paramId, slider);
+    
+    label.setColour (juce::Label::ColourIds::textColourId, juce::Colours::gold);
+    label.setFont (15.0f);
+    label.setJustificationType (juce::Justification::centred);
+    addAndMakeVisible (label);
+    
 }
